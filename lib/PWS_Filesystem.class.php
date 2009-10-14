3f803fd4d42a9c31b732b12513e73a8fd8b9ccc0
Dmitry Kasimtsev <dmitry@kasimtsev.com>
2009-10-15 02:45:37 +0300

    rewrited deleteDir function more laconic

--- lib/PWS_Filesystem.class.php
+++ lib/PWS_Filesystem.class.php
@@ -7,16 +7,14 @@ class PWS_Filesystem
     }
     public function deleteDir ($dir)
     {
-        $files = glob( $dir . '*', GLOB_MARK );
-        foreach( $files as $file ){
-            if(substr( $file, -1 ) == '/')
-                $this->deleteDir( $file );
-            else
-                unlink( $file );
+        foreach (glob($dir) as $file) {
+            if (is_dir($file)) {
+                $this->deleteDir("$file/*");
+                rmdir($file);
+            } else {
+                unlink($file);
+            }
         }
-
-        if (is_dir($dir)) 
-            rmdir( $dir ); 
 
     }
     
